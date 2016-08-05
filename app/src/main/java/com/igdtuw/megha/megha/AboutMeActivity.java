9eab588d17855c9a52a109851ad5b269ed0c2524
Megha Aggarwal <meghaaggarwal493@gmail.com>
2016-08-05 07:00:24 +0530

    Credits removed

--- app/src/main/java/com/igdtuw/megha/megha/AboutMeActivity.java
+++ app/src/main/java/com/igdtuw/megha/megha/AboutMeActivity.java
@@ -3,8 +3,6 @@ package com.igdtuw.megha.megha;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,7 +10,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.igdtuw.megha.megha.Activities.Project.Eventley;
@@ -26,7 +23,6 @@ import com.igdtuw.megha.megha.Fragments.ContactDetailsFragment;
 import com.igdtuw.megha.megha.Fragments.CoursesFragment;
 import com.igdtuw.megha.megha.Fragments.MiscellaneousFragment;
 import com.igdtuw.megha.megha.Fragments.ProjectsFragment;
-import com.igdtuw.megha.megha.Fragments.SpecialCreditsFragment;
 import com.igdtuw.megha.megha.Fragments.TechnicalSkillsFragment;
 
 public class AboutMeActivity extends AppCompatActivity
@@ -62,28 +58,6 @@ public class AboutMeActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.about_me, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -107,9 +81,6 @@ public class AboutMeActivity extends AppCompatActivity
             getSupportFragmentManager().beginTransaction().replace(R.id.coordinatorLayoutMainActivity, fragment).commit();
         } else if (id == R.id.nav_contact_details) {
             ContactDetailsFragment fragment = new ContactDetailsFragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.coordinatorLayoutMainActivity, fragment).commit();
-        } else if (id == R.id.nav_special_credits) {
-            SpecialCreditsFragment fragment = new SpecialCreditsFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.coordinatorLayoutMainActivity, fragment).commit();
         } else if (id == R.id.nav_miscellaneous) {
             MiscellaneousFragment fragment = new MiscellaneousFragment();
